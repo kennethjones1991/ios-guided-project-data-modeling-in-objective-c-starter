38b67396af73aac952ec8028a8da88d452c54795
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-21 18:40:56 -0700

    Added first responder class

---
+++ Quakes-Objc/FirstResponder.m
@@ -0,0 +1,13 @@
+//
+//  FirstResponder.m
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/21/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "FirstResponder.h"
+
+@implementation FirstResponder
+
+@end
