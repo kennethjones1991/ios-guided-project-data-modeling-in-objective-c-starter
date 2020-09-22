787b336fc707815b388628375fa9ddd189b0a866
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-21 20:29:43 -0700

    Added QuakeResults

---
+++ Quakes-Objc/QuakeResults.h
@@ -0,0 +1,17 @@
+//
+//  QuakeResults.h
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/21/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuakeResults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quakes-Objc/QuakeResults.m
@@ -0,0 +1,13 @@
+//
+//  QuakeResults.m
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/21/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "QuakeResults.h"
+
+@implementation QuakeResults
+
+@end
