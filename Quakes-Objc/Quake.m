d9d20d7b1d9ad5ef8b9c0edf8d32dc83efeec12e
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-21 19:18:13 -0700

    Added Quake model class

---
+++ Quakes-Objc/Quake.m
@@ -0,0 +1,13 @@
+//
+//  Quake.m
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/21/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "Quake.h"
+
+@implementation Quake
+
+@end
