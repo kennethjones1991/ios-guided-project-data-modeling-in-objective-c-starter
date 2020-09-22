2c0d9024159ba2d0bcdd5303d3a6e72f57ea6fd3
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-21 19:10:44 -0700

    Switched to using LSILog for simpler log messages

--- Quakes-Objc/ViewController.m
+++ Quakes-Objc/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LSILog.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSLog(@"Hey Quakes!");
 }
 
 
