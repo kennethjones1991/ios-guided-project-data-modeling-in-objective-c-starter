a78767cfb2d697f63abd9ef1b3928cf161775a33
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-21 19:28:14 -0700

    Added an implementation to the Quake model

--- Quakes-Objc/Quake.m
+++ Quakes-Objc/Quake.m
@@ -10,4 +10,16 @@
 
 @implementation Quake
 
+- (instancetype)initWithMagnitude:(double)aMagnitude place:(NSString *)aPlace time:(NSDate *)aTime latitude:(double)aLatitude longitude:(double)aLongitude
+{
+    if (self = [super init]) {
+        _magnitude = aMagnitude;
+        _place = aPlace.copy;
+        _time = aTime;
+        _latitude = aLatitude;
+        _longitude = aLongitude;
+    }
+    return self;
+}
+
 @end
