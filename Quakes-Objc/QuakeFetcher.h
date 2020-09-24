3f94069cd4d3cb0a12207138add3cc95a82efd64
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-23 19:44:00 -0700

    Added Quake Fetcher class

---
+++ Quakes-Objc/QuakeFetcher.h
@@ -0,0 +1,17 @@
+//
+//  QuakeFetcher.h
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuakeFetcher : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quakes-Objc/QuakeFetcher.m
@@ -0,0 +1,13 @@
+//
+//  QuakeFetcher.m
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "QuakeFetcher.h"
+
+@implementation QuakeFetcher
+
+@end
