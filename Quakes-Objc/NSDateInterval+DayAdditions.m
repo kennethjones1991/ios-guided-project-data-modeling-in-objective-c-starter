d9366f311cc94c4b0f3efebcc652e4362117f4dd
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-09-23 19:19:46 -0700

    Added a category on NSDateInterval named DayAdditions

---
+++ Quakes-Objc/NSDateInterval+DayAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSDateInterval+DayAdditions.m
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "NSDateInterval+DayAdditions.h"
+
+@implementation NSDateInterval (DayAdditions)
+
+@end
